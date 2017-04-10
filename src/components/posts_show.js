9a5390dab67e25a03da394c617c8bc525c6b0a28
strawhatasif <aafasih@gmail.com>
2017-04-10 15:39:45 -0500

    more updates after completing final lessons for this app

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+    
+    static contextTypes = {
+        router: PropTypes.object
+    };
     
     componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
+
+
+    //on a successful delete of a blog post - navigate user back to index
+    //navigate by calling this.context.router.push with
+    //the new path to navigate to.        
+            
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id)
+            .then(() => { this.context.router.push('/'); });
+    }
     
-    //loading text is needed only if the post is null or takes some time to retrieve
+    //loading text is needed only if the post is null or takes some time to retrieve (can be subsituted with a spinner, etc)
     render() {
         const { post } = this.props;
         
@@ -17,6 +32,13 @@ class PostsShow extends Component {
         }
         return (
             <div>
+                <Link to="/">Back to Index</Link>
+                <button 
+                    onClick={this.onDeleteClick.bind(this)}
+                    className="btn btn-danger pull-xs-right">
+            
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -29,5 +51,5 @@ function mapStateToProps(state) {
     return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps,{ fetchPost })(PostsShow);
+export default connect(mapStateToProps,{ fetchPost, deletePost })(PostsShow);
 
